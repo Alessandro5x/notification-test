4e17175269c7853327c528d6b74f5aa8d34610e2
Alessandro <alessandro5@live.com>
2024-05-22 14:13:03 -0300

    feat: frontend folders structure and first implementation of App.js and index.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NotificationForm from './components/NotificationForm';
+import LogHistory from './components/LogHistory';
+
+function App() {
+    return (
+        <div className="App">
+            <NotificationForm />
+            <LogHistory />
+        </div>
+    );
+}
+
+export default App;
